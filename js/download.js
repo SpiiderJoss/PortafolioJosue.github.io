495a3980cf7390e2556781b572f71e6245a093c9
SpiiderJoss <montalvanzavala@gmail.com>
2024-06-26 12:50:47 -0600

    primer commit de pagina

---
+++ js/download.js
@@ -0,0 +1,28 @@
+document.getElementById('downloadCvBtn').addEventListener('click', function(event) {
+    event.preventDefault(); // Previene el comportamiento por defecto del enlace
+  
+    const link = document.createElement('a');
+    link.href = 'Josu CV español.pdf';
+    link.download = 'CVJosueMontalvanZavala.pdf';
+  
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  
+    // Aquí puedes agregar cualquier otra lógica que necesites, por ejemplo, registrar la descarga
+  });
+
+  document.getElementById('downloadCvBtn1').addEventListener('click', function(event) {
+    event.preventDefault(); // Previene el comportamiento por defecto del enlace
+  
+    const link = document.createElement('a');
+    link.href = 'Josu CV español.pdf';
+    link.download = 'CVJosueMontalvanZavala.pdf';
+  
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  
+    // Aquí puedes agregar cualquier otra lógica que necesites, por ejemplo, registrar la descarga
+  });
+  
