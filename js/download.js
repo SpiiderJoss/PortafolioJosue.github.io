6d3160678870d03e985b3e4ac9660270d73a16c6
SpiiderJoss <montalvanzavala@gmail.com>
2024-12-07 22:26:04 -0600

    Nuevo cv Actualizaado 7.12..2024

--- js/download.js
+++ js/download.js
@@ -2,7 +2,7 @@ document.getElementById('downloadCvBtn').addEventListener('click', function(even
     event.preventDefault(); // Previene el comportamiento por defecto del enlace
   
     const link = document.createElement('a');
-    link.href = 'Josu CV español.pdf';
+    link.href = 'CV_Montalván_Zavala_Josué.pdf';
     link.download = 'CVJosueMontalvanZavala.pdf';
   
     document.body.appendChild(link);
